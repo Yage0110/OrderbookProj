b86f617a7db8fb9c835aa799dc9c462a20ea3472
Agyei Bacchus <151400661+Yage0110@users.noreply.github.com>
2024-09-04 01:28:13 -0400

    upload builder and test files for testing

---
+++ build/CMakeFiles/OPTIONSTRADINGBOOK.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPTIONSTRADINGBOOK.dir/Orderbook.cpp.o"
+  "CMakeFiles/OPTIONSTRADINGBOOK.dir/Orderbook.cpp.o.d"
+  "CMakeFiles/OPTIONSTRADINGBOOK.dir/test_include.cpp.o"
+  "CMakeFiles/OPTIONSTRADINGBOOK.dir/test_include.cpp.o.d"
+  "OPTIONSTRADINGBOOK"
+  "OPTIONSTRADINGBOOK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OPTIONSTRADINGBOOK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/agyeibacchus/CSCI/OptionsTradingBook
+# Build directory: /Users/agyeibacchus/CSCI/OptionsTradingBook/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(OPTIONSTRADINGBOOK "/Users/agyeibacchus/CSCI/OptionsTradingBook/build/OPTIONSTRADINGBOOK")
+set_tests_properties(OPTIONSTRADINGBOOK PROPERTIES  _BACKTRACE_TRIPLES "/Users/agyeibacchus/CSCI/OptionsTradingBook/CMakeLists.txt;32;add_test;/Users/agyeibacchus/CSCI/OptionsTradingBook/CMakeLists.txt;0;")
